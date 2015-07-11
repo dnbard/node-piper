30ec2544143c2db4ad15d5c3ed7e0a53ce8fb412
Alex Bardanov <dnbard@gmail.com>
2015-07-11 18:08:24 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var request = require('request'),
+    express = require('express'),
+    apiUrl = process.env.URL.trim(),
+    port = process.ENV.PORT || 9000;
+
+if (typeof apiUrl !== 'string' || apiUrl.length === 0){
+    throw new Error('Env variable URL must be specified');
+}
+
+function endpointProxy(req, res){
+    var url = apiUrl + req.originalUrl;
+    console.log(url);
+    req.pipe(request(url)).pipe(res);
+}
+
+var app = express();
+app.listen(port, function () {
+    app.use(endpointProxy);
+});
