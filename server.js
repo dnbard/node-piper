d7da4d37df03450cbc20f6b645bfa5fb07482308
Alex Bardanov <dnbard@gmail.com>
2015-07-12 12:54:48 +0300

    url FILTER regexp added as optional env param

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 var request = require('request'),
     express = require('express'),
     apiUrl = process.env.TARGET_URL.trim(),
-    port = process.env.PORT || 9000;
+    port = process.env.PORT || 9000,
+    filterString = process.env.FILTER,
+    filter;
 
 if (typeof apiUrl !== 'string' || apiUrl.length === 0){
     throw new Error('Env variable URL must be specified');
 }
 
+if (filterString){
+    filter = new RegExp(filterString);
+    console.log('Filter - %s', filterString);
+}
+
 function endpointProxy(req, res){
     var url = apiUrl + req.originalUrl;
-    console.log(url);
-    req.pipe(request(url)).pipe(res);
+
+    if (filter.test(url)){
+        console.log('Blocked - %s', url);
+        return res.status(500).send();
+    }
+
+    console.log('Sent - %s', url);
+    return req.pipe(request(url)).pipe(res);
 }
 
 var app = express();
